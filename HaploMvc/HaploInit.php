212812b8dda53d188901908e9ac9952827da3da7
Ed Eliot <me@ejeliot.com>
2014-03-23 17:18:38 +0000

    switched to composer autoloader

--- HaploMvc/HaploInit.php
+++ HaploMvc/HaploInit.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Copyright (C) 2008-2013, Brightfish Software Limited
+ * Copyright (C) 2008-2014, Brightfish Software Limited
  **/
 
 namespace HaploMvc;
 
 require APP_BASE.'/HaploMvc/Exception/HaploExceptions.php';
-require APP_BASE.'/HaploMvc/HaploLoader.php';
 require APP_BASE.'/vendor/autoload.php';
 
 // this can be disabled after successful setup
